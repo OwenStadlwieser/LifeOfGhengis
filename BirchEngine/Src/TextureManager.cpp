825ad4db5bf0afdeed8dad05c6d552fc9604228f
OwenStadlwieser <47876306+OwenStadlwieser@users.noreply.github.com>
2021-12-05 18:34:03 -0700

    fix rendering

--- BirchEngine/Src/TextureManager.cpp
+++ BirchEngine/Src/TextureManager.cpp
@@ -24,30 +24,40 @@ void TextureManager::Draw(SDL_Texture* tex, SDL_Rect src, SDL_Rect dest, SDL_Ren
 void TextureManager::DrawTile(SDL_Texture* tex, SDL_Rect src, SDL_Rect dest, SDL_RendererFlip flip, float targetx, float targety, float positionx, float positiony)
 {
 	float dist = powf(powf(targetx - positionx, 2) + powf(targety - positiony, 2), 0.5);
-	SDL_SetTextureBlendMode(tex, SDL_BLENDMODE_BLEND);
-	uint8_t colorMod = 60;
-	if (dist > 255) {
-		dist = 255;
+	if (dist < 1500) {
+		SDL_SetTextureBlendMode(tex, SDL_BLENDMODE_BLEND);
+		uint8_t colorMod = 60;
+		if (dist > 255) {
+			dist = 255;
+		}
+		else if (dist < 95) {
+			dist = 95;
+		}
+		SDL_SetTextureColorMod(tex, colorMod, colorMod, colorMod);
+		SDL_SetTextureAlphaMod(tex, dist);
+		SDL_RenderCopyEx(Game::renderer, tex, &src, &dest, NULL, NULL, flip);
 	}
-	else if (dist < 95) {
-		dist = 95;
+	else {
+		SDL_RenderCopyEx(Game::renderer, tex, &src, &dest, NULL, NULL, flip);
 	}
-	SDL_SetTextureColorMod(tex, colorMod, colorMod, colorMod);
-	SDL_SetTextureAlphaMod(tex, dist);
-	SDL_RenderCopyEx(Game::renderer, tex, &src, &dest, NULL, NULL, flip);
 };
 void TextureManager::DrawSprite(SDL_Texture* tex, SDL_Rect src, SDL_Rect dest, SDL_RendererFlip flip, float targetx, float targety, float positionx, float positiony)
 {
 	float dist = powf(powf(targetx - positionx, 2) + powf(targety - positiony, 2), 0.5);
-	SDL_SetTextureBlendMode(tex, SDL_BLENDMODE_BLEND);
-	uint8_t colorMod = 60;
-	if (dist > 255 *2.2) {
-		dist = 255 *2.2;
+	if (dist < 1500) {
+		SDL_SetTextureBlendMode(tex, SDL_BLENDMODE_BLEND);
+		uint8_t colorMod = 60;
+		if (dist > 255 * 2.2) {
+			dist = 255 * 2.2;
+		}
+		else if (dist < 95) {
+			dist = 95;
+		}
+		SDL_SetTextureColorMod(tex, colorMod, colorMod, colorMod);
+		SDL_SetTextureAlphaMod(tex, 255 - (dist / 2.2));
+		SDL_RenderCopyEx(Game::renderer, tex, &src, &dest, NULL, NULL, flip);
 	}
-	else if (dist < 95) {
-		dist = 95;
+	else {
+		SDL_RenderCopyEx(Game::renderer, tex, &src, &dest, NULL, NULL, flip);
 	}
-	SDL_SetTextureColorMod(tex, colorMod, colorMod, colorMod);
-	SDL_SetTextureAlphaMod(tex, 255 - (dist/ 2.2));
-	SDL_RenderCopyEx(Game::renderer, tex, &src, &dest, NULL, NULL, flip);
 };
